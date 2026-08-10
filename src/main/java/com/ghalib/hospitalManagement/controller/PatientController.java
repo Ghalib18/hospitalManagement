de1414705d003355244ac1e270b147e500e4b517
Ghalib18 <ghalibhussain733@gmail.com>
2026-08-10 10:57:38 +0530

    Spring Security -1

--- src/main/java/com/ghalib/hospitalManagement/controller/PatientController.java
+++ src/main/java/com/ghalib/hospitalManagement/controller/PatientController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@RequestMapping("/public")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class PatientController {
     private final PatientService patientService;
